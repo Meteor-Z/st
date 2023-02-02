e482dd50e8d778660f832e90788880300fd47530
Meteor_Z <3030218083@qq.com>
2023-02-02 19:17:48 +0800

    最新版本

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Jetbrains nerd font mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "JetBrains mono:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
